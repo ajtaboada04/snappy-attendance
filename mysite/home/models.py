4dd8714d00359f4f120dce2b415b2aa98f9288ad
ramcav <110005362+ramcav@users.noreply.github.com>
2023-11-21 15:32:04 +0100

    "Made models"

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import User
+
+class Professor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # Link professor account to a respective user
+    name = models.CharField(max_length=255)
+    
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # Link professor account to a respective user  
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=255)
+    semester = models.CharField(max_length=255)
+    students = models.ManyToManyField(Student)
+    professors = models.ManyToManyField(Professor)
+    
+class Attendance(models.Model):
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    
+    STATUS_CHOICES = [
+        ('present', 'Present'),
+        ('late', 'Late'),
+        ('absent', 'Absent'),
+    ]
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+
+class Session(models.Model):
+    course = models.OneToOneField(Course, on_delete=models.CASCADE)
+    attendance = models.OneToOneField(Attendance, on_delete=models.CASCADE)
+    
+class Class(models.Model):
+    course = models.OneToOneField(Course, on_delete=models.CASCADE)
+    attendance = models.ForeignKey(Attendance, on_delete=models.CASCADE)
