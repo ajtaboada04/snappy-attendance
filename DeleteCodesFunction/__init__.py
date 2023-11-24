aa41f3938897b6c0d94e3681396a82e9444ebb5a
rorosaga <rodrigo.sagastegui9@gmail.com>
2023-11-24 20:12:12 +0100

    azure functions working now

--- DeleteCodesFunction/__init__.py
+++ DeleteCodesFunction/__init__.py
@@ -1,12 +1,13 @@
 import os
 import datetime
+import logging
 import azure.functions as func
 from azure.cosmosdb.table.tableservice import TableService
 
 def delete_code_from_table(table_service, table_name, partition_key, row_key):
     table_service.delete_entity(table_name, partition_key, row_key)
 
-def main (req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         # Initialize table service
         table_service = TableService(connection_string=os.environ['AzureWebJobsStorage'])
@@ -16,16 +17,20 @@ def main (req: func.HttpRequest) -> func.HttpResponse:
         # Get all entities in the table
         entities = table_service.query_entities(table_name, filter=f"PartitionKey eq '{partition_key}'")
 
-        # Get the current time
-        current_time = datetime.datetime.utcnow()
+        # Get the current time in UTC
+        current_time = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)
 
         # Iterate over entities and delete codes older than 30 seconds
         for entity in entities:
-            code_timestamp = entity.Timestamp
-            time_difference = current_time - code_timestamp
+            # Make the entity timestamp "offset-aware"
+            code_timestamp_aware = entity.Timestamp.replace(tzinfo=datetime.timezone.utc)
+
+            time_difference = current_time - code_timestamp_aware
             if time_difference.total_seconds() > 30:
                 delete_code_from_table(table_service, table_name, partition_key, entity.RowKey)
-                print(f"Deleted code: {entity.RowKey}")
+                log_msg = f"Deleted code: {entity.RowKey}"
+                logging.info(log_msg)
+
         return func.HttpResponse("Cleanup completed successfully.", status_code=200)
     except Exception as e:
-        print(f"An error occurred: {str(e)}")
+        return func.HttpResponse(f"An error occurred: {str(e)}", status_code=500)
