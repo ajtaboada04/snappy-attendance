3c1203a719c213cea391ca4c5c7ccdba6deac769
rorosaga <rodrigo.sagastegui9@gmail.com>
2023-11-25 00:50:08 +0100

    tried fizing ValidateCodeFunction

--- ValidateCodeFunction/__init__.py
+++ ValidateCodeFunction/__init__.py
@@ -2,6 +2,7 @@ import os
 import json
 import azure.functions as func
 from azure.cosmosdb.table.tableservice import TableService
+from azure.cosmosdb.table.models import Entity
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
@@ -14,23 +15,23 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         # Retrieve the latest code from Azure Table Storage
         table_service = TableService(connection_string=os.environ['AzureWebJobsStorage'])
         table_name = 'TempCodes'
-        partition_key = 'Codes'
         
         # Retrieve the latest entity (code) from the table
-        latest_entity = table_service.query_entities(table_name, filter=f"PartitionKey eq '{partition_key}'", top=1, orderby="Timestamp desc").items()
+        result = table_service.query_entities(table_name, filter="PartitionKey eq 'Codes'", top=1, order_by="Timestamp desc")
 
-        if not latest_entity:
+
+        if result.items:
+            latest_entity = result.items[0]
+
+            if received_code == latest_entity['RowKey']:
+                # Code is valid
+                return func.HttpResponse("true", status_code=200)
+            else:
+                # Code is invalid
+                return func.HttpResponse("false", status_code=400)
+        else:
             # No code found in the table
             return func.HttpResponse("No code found in the table.", status_code=400)
-
-        latest_entity = latest_entity[0]
-
-        if received_code == latest_entity.RowKey:
-            # Code is valid
-            return func.HttpResponse("true", status_code=200)
-        else:
-            # Code is invalid
-            return func.HttpResponse("false", status_code=400)
 
     except Exception as e:
         return func.HttpResponse(f"An error occurred: {str(e)}", status_code=500)
