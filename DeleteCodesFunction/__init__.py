f68cc9a69de8e74f75b317c455c3a6a97bbda120
rorosaga <rodrigo.sagastegui9@gmail.com>
2023-11-26 13:48:27 +0100

    ValidateCodeFunction and Delete are now working

--- DeleteCodesFunction/__init__.py
+++ DeleteCodesFunction/__init__.py
@@ -20,17 +20,23 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         # Get the current time in UTC
         current_time = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)
 
-        # Iterate over entities and delete codes older than 30 seconds
-        for entity in entities:
-            # Make the entity timestamp "offset-aware"
-            code_timestamp_aware = entity.Timestamp.replace(tzinfo=datetime.timezone.utc)
+        if entities.items:
+            # Iterate over entities and delete codes older than 30 seconds
+            for entity in entities:
+                # Make the entity timestamp "offset-aware"
+                code_timestamp_aware = entity.Timestamp.replace(tzinfo=datetime.timezone.utc)
 
-            time_difference = current_time - code_timestamp_aware
-            if time_difference.total_seconds() > 30:
-                delete_code_from_table(table_service, table_name, partition_key, entity.RowKey)
-                log_msg = f"Deleted code: {entity.RowKey}"
-                logging.info(log_msg)
+                time_difference = current_time - code_timestamp_aware
+                if time_difference.total_seconds() > 30:
+                    delete_code_from_table(table_service, table_name, partition_key, entity.RowKey)
+                    log_msg = f"Deleted code: {entity.RowKey}"
+                    logging.info(log_msg)
 
-        return func.HttpResponse("Cleanup completed successfully.", status_code=200)
+            return func.HttpResponse("Cleanup completed successfully.", status_code=200)
+        
+        else:
+            # No code found in the table
+            return func.HttpResponse("No code found in the table.", status_code=400)
+        
     except Exception as e:
         return func.HttpResponse(f"An error occurred: {str(e)}", status_code=500)
--- ValidateCodeFunction/__init__.py
+++ ValidateCodeFunction/__init__.py
@@ -14,25 +14,27 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
         # Retrieve the latest code from Azure Table Storage
         table_service = TableService(connection_string=os.environ['AzureWebJobsStorage'])
-        table_name = 'TempCodes'
+        table_name = "TempCodes"
+        partition_key = "Codes"
         
-        # Retrieve the latest entity (code) from the table
-        result = table_service.query_entities(table_name, filter="PartitionKey eq 'Codes'", top=1, order_by="Timestamp desc")
+        entities = table_service.query_entities(table_name, filter=f"PartitionKey eq '{partition_key}'")
 
+        
+        if entities.items:
 
-        if result.items:
-            latest_entity = result.items[0]
+            for entity in entities:
 
-            if received_code == latest_entity['RowKey']:
-                # Code is valid
-                return func.HttpResponse("true", status_code=200)
-            else:
-                # Code is invalid
-                return func.HttpResponse("false", status_code=400)
+                if received_code == entity['RowKey']:
+                    # Code is valid
+                    return func.HttpResponse("true", status_code=200)
+            
+            return func.HttpResponse("false", status_code=200)
         else:
             # No code found in the table
             return func.HttpResponse("No code found in the table.", status_code=400)
 
     except Exception as e:
+        import traceback
+        traceback.print_exc()
         return func.HttpResponse(f"An error occurred: {str(e)}", status_code=500)
 
